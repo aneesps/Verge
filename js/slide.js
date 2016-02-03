cbc560db96af7841588ac2a7092def1756f24d75
anees <anees@qburst.com>
2016-02-03 16:49:19 +0530

    added media query for social links in footer

--- js/slide.js
+++ js/slide.js
@@ -6,16 +6,19 @@ $("#nav_bar").click(function() {
 
 $("#news_list").click(function() {
     $("#news_articles").addClass("slider_right");
+    
     $("#unclick_right").addClass("expand_right");
    $("#body_wrap").css({'overflow':"hidden"});
 });
 $("#unclick_left").click(function() {
     $("#nav_bar_list").removeClass("slider_left");
-    $("#unclick_left").removeClass("expand_left");
+    setTimeout(function(){
+    $("#unclick_left").removeClass("expand_left");},1000);
     $("#body_wrap").css({'overflow':""});
 });
 $("#unclick_right").click(function() {
     $("#news_articles").removeClass("slider_right");
-    $("#unclick_right").removeClass("expand_right");
+    setTimeout(function(){
+    $("#unclick_right").removeClass("expand_right");},1000);
     $("#body_wrap").css({'overflow':""});
 });
